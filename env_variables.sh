f0e14b601f9c2a826ec96ef4d8030d9f34544dba
MaianaBt <dellyarruda@gmail.com>
2018-11-16 23:56:54 -0300

    added gitignore

--- env_variables.sh
+++
@@ -1,5 +0,0 @@
-echo "Setting env variables up"
-export ENVIRONMENT=test
-export AWS_ACCESS_KEY_ID="AKIAICZ3KOVJVBBOVHHQ"
-export AWS_SECRET_ACCESS_KEY="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"
-echo "Done"
